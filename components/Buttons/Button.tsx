fda4f2f25f6a0eac8c8fc5983b11006a30d09fef
CREDO23 <thierrybakera12@gmail.com>
2023-01-15 23:49:19 +0200

    update login UI and responsiveness

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -1,36 +1,36 @@
-import { LoadingOutlined } from '@ant-design/icons';
-import { MouseEventHandler } from 'react';
+import { LoadingOutlined } from "@ant-design/icons";
+import { MouseEventHandler } from "react";
 
 interface Props {
-  disabled?: boolean;
-  onClick: MouseEventHandler;
-  style?: React.CSSProperties;
-  loading?: boolean;
-  name: string;
-  type?: 'default' | 'outlined';
+    disabled?: boolean;
+    onClick: MouseEventHandler;
+    style?: React.CSSProperties;
+    loading?: boolean;
+    name: string;
+    type?: "default" | "outlined";
 }
 
 export default function Button({
-  style,
-  loading,
-  disabled,
-  onClick,
-  name,
-  type,
+    style,
+    loading,
+    disabled,
+    onClick,
+    name,
+    type,
 }: Props): JSX.Element {
-  return (
-    <button
-      onClick={onClick}
-      disabled={disabled}
-      style={style}
-      className={`${
-        type == 'outlined'
-          ? `    text-custom-gray-700 bg-transparent border-custom-gray-100`
-          : ` bg-gray-100 border-gray-100 shadow  relative transition-all hover:after:opacity-10  hover:after:h-full hover:after:w-full hover:after:bg-white hover:after:absolute hover:after:top-0 hover:after:left-0   hover:bg-gray-200`
-      }  border my-2 h-10 text-custom-lg w-full flex items-center justify-center rounded gap-3 px-2 py-1`}
-    >
-      {loading && <LoadingOutlined />}
-      {name}
-    </button>
-  );
+    return (
+        <button
+            onClick={onClick}
+            disabled={disabled}
+            style={style}
+            className={`${
+                type == "outlined"
+                    ? `    text-custom-gray-700 bg-transparent border-custom-gray-100`
+                    : ` bg-gray-100 border-gray-100 shadow  relative transition-all hover:after:opacity-10  hover:after:h-full hover:after:w-full hover:after:bg-white hover:after:absolute hover:after:top-0 hover:after:left-0   hover:bg-gray-200`
+            }  border my-2 h-10 text-custom-lg w-full flex items-center justify-center rounded gap-3 px-2 py-1`}
+        >
+            {loading && <LoadingOutlined />}
+            {name}
+        </button>
+    );
 }
