42d1deccaf6da4c02706d2627be6616285910900
CREDO23 <thierrybakera12@gmail.com>
2023-02-10 20:57:43 +0200

    remove old register form

--- types/index.ts
+++
@@ -1,5 +0,0 @@
-import type { ButtonProps } from "./button";
-import type { CarouselProps } from "./carousels";
-import type { TextinputProps, SelectinputProps } from "./input";
-
-export { ButtonProps, TextinputProps, SelectinputProps, CarouselProps };
